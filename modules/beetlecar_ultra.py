6b8db0ebafa66cf6f669a24fcfb09693a0c38fc9
Thanaphon06 <68720136+Thanaphon06@users.noreply.github.com>
2023-06-30 14:42:08 +0700

    Add files via upload

---
+++ modules/beetlecar_ultra.py
@@ -0,0 +1,26 @@
+from machine import Pin
+from machine import time_pulse_us
+from time import sleep_us
+
+ULTRASONIC_TRIGGER_PIN = 2
+ULTRASONIC_ECHO_PIN = 15
+
+def read(trig_pin, echo_pin):
+    trigger = Pin(trig_pin, mode=Pin.OUT)
+    echo = Pin(echo_pin, mode=Pin.IN)
+
+    trigger.value(0)
+    sleep_us(5)
+    trigger.value(1)
+    sleep_us(10)
+    trigger.value(0)
+
+    try:
+        pulse_time = time_pulse_us(echo, 1, 1000000)
+        d = (pulse_time / 2) / 29.1
+        return int(d) if d < 400 else -1
+    except OSError as ex:
+        return -1
+
+def distance():
+    return read(ULTRASONIC_TRIGGER_PIN, ULTRASONIC_ECHO_PIN)
