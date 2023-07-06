5dfe3fdd2fb9624f40dc6f0c945b202291270cc5
Thanaphon06 <68720136+Thanaphon06@users.noreply.github.com>
2023-07-06 13:33:45 +0700

    Update buzzer

--- blocks/blocks.js
+++ blocks/blocks.js
@@ -518,6 +518,31 @@ Blockly.defineBlocksWithJsonArray([
   "tooltip": "",
   "helpUrl": ""
 },
+    {
+        "type": "buz_play_music",
+            "message0": "Play Music🎵 : %1",
+            "args0":[
+            {
+              "type": "field_dropdown",
+              "name": "music",
+              "options": [
+                [Blockly.Msg.happy , "1"],
+                [Blockly.Msg.darth_vader , "2"],
+                [Blockly.Msg.jingle_bell , "3"],                
+                [Blockly.Msg.mary_little_lamb , "4"],
+                [Blockly.Msg.bitsy_spider , "5"]
+              ]
+                
+            }
+          ], 
+          "inputsInline": true,
+          "previousStatement": null,
+          "nextStatement": null,
+          "colour": "#27AE60",
+          "tooltip": "",
+          "helpUrl": ""
+
+    },
 //Buzzer End /////////////////////////////////
 
 //LED Start //////////////////////////////////
