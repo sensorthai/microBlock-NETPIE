1b74841a9e0c9acf7ef23753f70813e5c4705a16
Thanaphon06 <68720136+Thanaphon06@users.noreply.github.com>
2023-07-06 13:35:30 +0700

    Update buzzer

--- blocks/generators.js
+++ blocks/generators.js
@@ -406,6 +406,28 @@
     var code = `${buzzerWriteFunction()}(23, duty=0)\n`;
     return code;
   };
+Blockly.Python['buz_play_music'] = function(block) {
+  Blockly.Python.definitions_['from_machine_import_pin'] = 'from machine import Pin,PWM';
+  Blockly.Python.definitions_['from_time_import_sleep'] = 'from time import sleep';
+
+    Blockly.Python.definitions_['import_beetlecar'] = 'import beetlecar';
+      var select_song = block.getFieldValue('music');
+
+      code = '';;
+      if(select_song == 1){
+        code = `for i in beetlecar.happy_birthday:\n    beetlecar.buzzerWrite(23, freq=beetlecar.scale5[i], stop=0.3)\n    sleep(0.1)\n`;
+      }else if(select_song == 2){
+        code = `for i in beetlecar.darth_vader:\n    beetlecar.buzzerWrite(23, freq=beetlecar.scale5[i], stop=0.5)\n    sleep(0.1)\n`;
+      }else if(select_song == 3){
+        code = `for i in beetlecar.jingle_bell:\n    beetlecar.buzzerWrite(23, freq=beetlecar.scale5[i], stop=0.3)\n    sleep(0.1)\n`;
+      }else if(select_song == 4){
+        code = `for i in beetlecar.mary_little_lamb:\n    beetlecar.buzzerWrite(23, freq=beetlecar.scale5[i], stop=0.3)\n    sleep(0.1)\n`;
+      }else if(select_song == 5){
+        code = `for i in beetlecar.bitsy_spider:\n    beetlecar.buzzerWrite(23, freq=beetlecar.scale5[i], stop=0.3)\n    sleep(0.1)\n`;
+      }
+      return code;
+    };
+  
   // buzzer End /////////////////////////////////
   
   // LED Start //////////////////////////////////
