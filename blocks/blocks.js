d3be40059a10ca6c4e91784eacf07f8b9e09633e
Thanaphon06 <68720136+Thanaphon06@users.noreply.github.com>
2023-07-12 10:19:25 +0700

    Update blocks.js

--- blocks/blocks.js
+++ blocks/blocks.js
@@ -752,5 +752,78 @@ Blockly.defineBlocksWithJsonArray([
     "tooltip": "",
     "helpUrl": ""
   },
+ {
+  "type":"ir_com",
+  "message0": "Put Button %1",
+  "args0":[
+    {
+      "type" : "field_dropdown",
+      "name" : "ir_but",
+      "options" : [
+        [
+          Blockly.Msg.but1, "1"
+        ],
+        [
+          Blockly.Msg.but2 , "2"
+        ],
+        [
+          Blockly.Msg.but3 , "3"
+        ],
+        [
+          Blockly.Msg.but4 , "4"
+        ],
+        [
+          Blockly.Msg.but5 , "5"
+        ],
+        [
+          Blockly.Msg.but6 , "6"
+        ],
+        [
+          Blockly.Msg.but7 , "7"
+        ],
+        [
+          Blockly.Msg.but8 , "8"
+        ],
+        [
+          Blockly.Msg.but9 , "9"
+        ],
+        [
+          Blockly.Msg.but0 , "0"
+        ],
+        [
+          Blockly.Msg.buttag , "#"
+        ],
+        [
+          Blockly.Msg.butstar , "*"
+        ],
+        [
+          Blockly.Msg.butup , "Up"
+        ],
+        [
+          Blockly.Msg.butdown , "Down"
+        ],
+        [
+          Blockly.Msg.butleft , "Left"
+        ],
+        [
+          Blockly.Msg.butright , "Right"
+        ],
+        [
+          Blockly.Msg.butokay , "Ok"
+        ],
+        
+
+      ]
+    }
+  ],
+"output": ["Number", "Boolean"],
+"inputsInline": true,
+//"peviousStatement": null,
+//"nextStatement": null,
+"colour": "#FE0000",
+"tooltip": "",
+"helpUrl": ""
+
+},
   //IR_END
 ]);
