0f0567c4c6010e06f76d0d9bb4cbc4a171ca195e
Thanaphon06 <68720136+Thanaphon06@users.noreply.github.com>
2023-07-13 13:51:13 +0700

    Update motor generate

--- blocks/generators.js
+++ blocks/generators.js
@@ -1,177 +1,83 @@
 
     //Motor Strat //////////////////////////
 
-    Blockly.Python['stopMotor'] = function(block) {
-        Blockly.Python.definitions_['import_beetlecar'] = 'import beetlecar';
-      
-        var code = 'beetlecar.beetlecarmotor.stop()\n';
-       
-        return code;
-      };
-    
-    Blockly.Python['moveAward'] = function(block) {
-      Blockly.Python.definitions_['import_beetlecar'] = 'import beetlecar';
-      
-      var speed = block.getFieldValue('speed');
-      var code = `beetlecar.beetlecarmotor.forward(${speed})\n`;
-      
-      return code;
-    };
-    
-    Blockly.Python['moveBack'] = function(block) {
-      Blockly.Python.definitions_['import_beetlecar'] = 'import beetlecar';
-      var speed = block.getFieldValue('speed');
-      var code = `beetlecar.beetlecarmotor.backward(${speed})\n`;
-    
-      
-      return code;
-    };
-    
-    Blockly.Python['moveLeft'] = function(block) {
-      Blockly.Python.definitions_['import_beetlecar'] = 'import beetlecar';
-      var speed = block.getFieldValue('speed');
-      var code = `beetlecar.beetlecarmotor.left(${speed})\n`;
-    
-      
-      return code;
-    };
-    
-    Blockly.Python['moveRight'] = function(block) {
-      Blockly.Python.definitions_['import_beetlecar'] = 'import beetlecar';
-      var speed = block.getFieldValue('speed');
-      var code = `beetlecar.beetlecarmotor.right(${speed})\n`;
-    
-      
-      return code;
-    };
-      
-      Blockly.Python['Forward'] = function(block) {
-        Blockly.Python.definitions_['import_machine'] = 'import machine';
-        Blockly.Python.definitions_['import_time'] = 'import time';
-        Blockly.Python.definitions_['from_machine_import_pin'] = 'from machine import Pin';
-        Blockly.Python.definitions_['from_machine_import_pwm'] = 'from machine import PWM';
-        Blockly.Python.definitions_['from_time_import_sleep'] = 'from time import sleep';
-        
-        Blockly.Python.definitions_['LMotor'] = 'LMotor = machine.Pin(27, machine.Pin.OUT);';
-        Blockly.Python.definitions_['LMotor2'] = 'LMotor2 = machine.Pin(14, machine.Pin.OUT);';
-        Blockly.Python.definitions_['RMotor'] = 'RMotor = machine.Pin(25, machine.Pin.OUT);';
-        Blockly.Python.definitions_['RMotor2'] = 'RMotor2 = machine.Pin(26, machine.Pin.OUT);';
-        Blockly.Python.definitions_['pwm1'] = 'pwm1 = machine.PWM(LMotor)';
-        Blockly.Python.definitions_['pwm2'] = 'pwm2 = machine.PWM(RMotor)';
-        Blockly.Python.definitions_['pwm3'] = 'pwm3 = machine.PWM(LMotor2)';
-        Blockly.Python.definitions_['pwm4'] = 'pwm4 = machine.PWM(RMotor2)';
-      
-        var speed = block.getFieldValue('speed');
-        var code = `Forward(${speed})\n`;
-        var functionName = Blockly.Python.provideFunction_(
-          'Forward',
-          ['def ' + Blockly.Python.FUNCTION_NAME_PLACEHOLDER_ + '(speed):',
-          '  LMotor.on()',
-          '  pwm1.duty(speed)',
-          '  RMotor.on()',
-          '  pwm2.duty(speed)',
-          '  LMotor2.off()',
-          '  pwm3.duty(0)',
-          '  RMotor2.off()',
-          '  pwm4.duty(0)']);
-        return code;
-      };
-      
-      Blockly.Python['Backward'] = function(block) {
-        Blockly.Python.definitions_['import_machine'] = 'import machine';
-        Blockly.Python.definitions_['import_time'] = 'import time';
-        Blockly.Python.definitions_['from_machine_import_pin'] = 'from machine import Pin';
-        Blockly.Python.definitions_['from_machine_import_pwm'] = 'from machine import PWM';
-        Blockly.Python.definitions_['from_time_import_sleep'] = 'from time import sleep';
-        
-        Blockly.Python.definitions_['LMotor'] = 'LMotor = machine.Pin(27, machine.Pin.OUT);';
-        Blockly.Python.definitions_['LMotor2'] = 'LMotor2 = machine.Pin(14, machine.Pin.OUT);';
-        Blockly.Python.definitions_['RMotor'] = 'RMotor = machine.Pin(25, machine.Pin.OUT);';
-        Blockly.Python.definitions_['RMotor2'] = 'RMotor2 = machine.Pin(26, machine.Pin.OUT);';
-        Blockly.Python.definitions_['pwm1'] = 'pwm1 = machine.PWM(LMotor)';
-        Blockly.Python.definitions_['pwm2'] = 'pwm2 = machine.PWM(RMotor)';
-        Blockly.Python.definitions_['pwm3'] = 'pwm3 = machine.PWM(LMotor2)';
-        Blockly.Python.definitions_['pwm4'] = 'pwm4 = machine.PWM(RMotor2)';
-      
-        var speed = block.getFieldValue('speed');
-        var code = `Backward(${speed})\n`;
-        var functionName = Blockly.Python.provideFunction_(
-          'Backward',
-          ['def ' + Blockly.Python.FUNCTION_NAME_PLACEHOLDER_ + '(speed):',
-          '  LMotor.off()',
-          '  pwm1.duty(0)',
-          '  RMotor.off()',
-          '  pwm2.duty(0)',
-          '  LMotor2.on()',
-          '  pwm3.duty(speed)',
-          '  RMotor2.on()',
-          '  pwm4.duty(speed)']);
-        return code;
-      };
-      
-      Blockly.Python['Left'] = function(block) {
-        Blockly.Python.definitions_['import_machine'] = 'import machine';
-        Blockly.Python.definitions_['import_time'] = 'import time';
-        Blockly.Python.definitions_['from_machine_import_pin'] = 'from machine import Pin';
-        Blockly.Python.definitions_['from_machine_import_pwm'] = 'from machine import PWM';
-        Blockly.Python.definitions_['from_time_import_sleep'] = 'from time import sleep';
-        
-        Blockly.Python.definitions_['LMotor'] = 'LMotor = machine.Pin(27, machine.Pin.OUT);';
-        Blockly.Python.definitions_['LMotor2'] = 'LMotor2 = machine.Pin(14, machine.Pin.OUT);';
-        Blockly.Python.definitions_['RMotor'] = 'RMotor = machine.Pin(25, machine.Pin.OUT);';
-        Blockly.Python.definitions_['RMotor2'] = 'RMotor2 = machine.Pin(26, machine.Pin.OUT);';
-        Blockly.Python.definitions_['pwm1'] = 'pwm1 = machine.PWM(LMotor)';
-        Blockly.Python.definitions_['pwm2'] = 'pwm2 = machine.PWM(RMotor)';
-        Blockly.Python.definitions_['pwm3'] = 'pwm3 = machine.PWM(LMotor2)';
-        Blockly.Python.definitions_['pwm4'] = 'pwm4 = machine.PWM(RMotor2)';
-      
-        var speed = block.getFieldValue('speed');
-        var code = `Left(${speed})\n`;
-        var functionName = Blockly.Python.provideFunction_(
-          'Left',
-          ['def ' + Blockly.Python.FUNCTION_NAME_PLACEHOLDER_ + '(speed):',
-          '  LMotor.off()',
-          '  pwm1.duty(0)',
-          '  RMotor.on()',
-          '  pwm2.duty(speed)',
-          '  LMotor2.off()',
-          '  pwm3.duty(0)',  
-          '  RMotor2.off()',
-          '  pwm4.duty(0)']);
-        return code;
-      };
-      
-      Blockly.Python['Right'] = function(block) {
-        Blockly.Python.definitions_['import_machine'] = 'import machine';
-        Blockly.Python.definitions_['import_time'] = 'import time';
-        Blockly.Python.definitions_['from_machine_import_pin'] = 'from machine import Pin';
-        Blockly.Python.definitions_['from_machine_import_pwm'] = 'from machine import PWM';
-        Blockly.Python.definitions_['from_time_import_sleep'] = 'from time import sleep';
-        
-        Blockly.Python.definitions_['LMotor'] = 'LMotor = machine.Pin(27, machine.Pin.OUT);';
-        Blockly.Python.definitions_['LMotor2'] = 'LMotor2 = machine.Pin(14, machine.Pin.OUT);';
-        Blockly.Python.definitions_['RMotor'] = 'RMotor = machine.Pin(25, machine.Pin.OUT);';
-        Blockly.Python.definitions_['RMotor2'] = 'RMotor2 = machine.Pin(26, machine.Pin.OUT);';
-        Blockly.Python.definitions_['pwm1'] = 'pwm1 = machine.PWM(LMotor)';
-        Blockly.Python.definitions_['pwm2'] = 'pwm2 = machine.PWM(RMotor)';
-        Blockly.Python.definitions_['pwm3'] = 'pwm3 = machine.PWM(LMotor2)';
-        Blockly.Python.definitions_['pwm4'] = 'pwm4 = machine.PWM(RMotor2)';
-      
-        var speed = block.getFieldValue('speed');
-        var code = `Right(${speed})\n`;
-        var functionName = Blockly.Python.provideFunction_(
-          'Right',
-          ['def ' + Blockly.Python.FUNCTION_NAME_PLACEHOLDER_ + '(speed):',
-          '  LMotor.on()',
-          '  pwm1.duty(speed)',
-          '  RMotor.off()',
-          '  pwm2.duty(0)',
-          '  LMotor2.off()',
-          '  pwm3.duty(0)',  
-          '  RMotor2.off()',
-          '  pwm4.duty(0)']);
-        return code;
-      };
+  Blockly.Python['stopMotor'] = function(block) {
+    Blockly.Python.definitions_['import_beetlecar'] = 'import beetlecar';
+  
+    var code = 'beetlecar.stop()\n';
+   
+    return code;
+  };
+
+Blockly.Python['moveAward'] = function(block) {
+  Blockly.Python.definitions_['import_beetlecar'] = 'import beetlecar';
+  
+  var speed = block.getFieldValue('speed');
+  var code = `beetlecar.forward_forsec(${speed})\n`;
+  
+  return code;
+};
+
+Blockly.Python['moveBack'] = function(block) {
+  Blockly.Python.definitions_['import_beetlecar'] = 'import beetlecar';
+  var speed = block.getFieldValue('speed');
+  var code = `beetlecar.backward_forsec(${speed})\n`;
+
+  
+  return code;
+};
+
+Blockly.Python['moveLeft'] = function(block) {
+  Blockly.Python.definitions_['import_beetlecar'] = 'import beetlecar';
+  var speed = block.getFieldValue('speed');
+  var code = `beetlecar.left_forsec(${speed})\n`;
+
+  
+  return code;
+};
+
+Blockly.Python['moveRight'] = function(block) {
+  Blockly.Python.definitions_['import_beetlecar'] = 'import beetlecar';
+  var speed = block.getFieldValue('speed');
+  var code = `beetlecar.right_forsec(${speed})\n`;
+
+  
+  return code;
+};
+
+Blockly.Python['Forward'] = function(block) {
+  Blockly.Python.definitions_['import_beetlecar'] = 'import beetlecar';
+
+var speed = block.getFieldValue('speed');
+var code = `beetlecar.forward(${speed})\n`;
+
+return code;
+};
+
+Blockly.Python['Backward'] = function(block) {
+  Blockly.Python.definitions_['import_beetlecar'] = 'import beetlecar';
+
+var speed = block.getFieldValue('speed');
+var code = `beetlecar.backward(${speed})\n`;
+
+return code;
+};
+
+Blockly.Python['Left'] = function(block) {
+  Blockly.Python.definitions_['import_beetlecar'] = 'import beetlecar';
+var speed = block.getFieldValue('speed');
+var code = `beetlecar.left(${speed})\n`;
+
+return code;
+};
+
+Blockly.Python['Right'] = function(block) {
+  Blockly.Python.definitions_['import_beetlecar'] = 'import beetlecar';
+var speed = block.getFieldValue('speed');
+var code = `beetlecar.right(${speed})\n`;
+
+return code;
+};
       //Motor End ///////////////////////////////////
       
       // servo Start ////////////////////////////////
