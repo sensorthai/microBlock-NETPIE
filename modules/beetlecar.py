1c57052f07a99cad20393f20cc30eaca492b8ae1
Thanaphon06 <68720136+Thanaphon06@users.noreply.github.com>
2023-09-05 15:13:51 +0700

    Update beetlecar.py

--- modules/beetlecar.py
+++ modules/beetlecar.py
@@ -21,12 +21,7 @@ import os
 
 
 
-#SERVOR STATE START
 
-def servo(serpin, angle):
-    PWM(Pin(serpin), freq=50).duty(int(25.57 + (((angle) / 180.0) * 102.3)))
-
-#SERVOR STATE END
 #button state
 buttonA_pin = 18
 buttonB_pin = 5
@@ -376,104 +371,6 @@ twinkle_twinkle = [0, 0, 6, 6, 9, 9, 6, 13, 5, 5, 4, 4, 2, 2, 0, 13, 6, 6, 5, 5,
 
 bitsy_spider = [9, 0, 0, 2, 4, 4, 13, 4, 2, 0, 2, 4, 2, 13, 4, 4, 5, 7, 13, 7, 5, 4, 5, 7, 4, 0, 0, 2, 4, 13, 4, 2, 0, 2, 4, 0, 13, 7, 7, 0, 0, 0, 2, 4, 4, 13, 4, 2, 0, 2, 4, 0]
 
-class SSD1306(framebuf.FrameBuffer):
-    def __init__(self, width, height, external_vcc):
-        self.width = width
-        self.height = height
-        self.external_vcc = external_vcc
-        self.pages = self.height // 8
-        self.buffer = bytearray(self.pages * self.width)
-        super().__init__(self.buffer, self.width, self.height, framebuf.MONO_VLSB)
-        self.init_display()
 
-    def init_display(self):
-        for cmd in (
-            SET_DISP | 0x00,  # off
-            # address setting
-            SET_MEM_ADDR,
-            0x00,  # horizontal
-            # resolution and layout
-            SET_DISP_START_LINE | 0x00,
-            SET_SEG_REMAP | 0x01,  # column addr 127 mapped to SEG0
-            SET_MUX_RATIO,
-            self.height - 1,
-            SET_COM_OUT_DIR | 0x08,  # scan from COM[N] to COM0
-            SET_DISP_OFFSET,
-            0x00,
-            SET_COM_PIN_CFG,
-            0x02 if self.width > 2 * self.height else 0x12,
-            # timing and driving scheme
-            SET_DISP_CLK_DIV,
-            0x80,
-            SET_PRECHARGE,
-            0x22 if self.external_vcc else 0xF1,
-            SET_VCOM_DESEL,
-            0x30,  # 0.83*Vcc
-            # display
-            SET_CONTRAST,
-            0xFF,  # maximum
-            SET_ENTIRE_ON,  # output follows RAM contents
-            SET_NORM_INV,  # not inverted
-            # charge pump
-            SET_CHARGE_PUMP,
-            0x10 if self.external_vcc else 0x14,
-            SET_DISP | 0x01,
-        ):  # on
-            self.write_cmd(cmd)
-        self.fill(0)
-        self.show()
-
-    def poweroff(self):
-        self.write_cmd(SET_DISP | 0x00)
-
-    def poweron(self):
-        self.write_cmd(SET_DISP | 0x01)
-
-    def contrast(self, contrast):
-        self.write_cmd(SET_CONTRAST)
-        self.write_cmd(contrast)
-
-    def invert(self, invert):
-        self.write_cmd(SET_NORM_INV | (invert & 1))
-
-    def show(self):
-        x0 = 0
-        x1 = self.width - 1
-        if self.width == 64:
-            # displays with width of 64 pixels are shifted by 32
-            x0 += 32
-            x1 += 32
-        self.write_cmd(SET_COL_ADDR)
-        self.write_cmd(x0)
-        self.write_cmd(x1)
-        self.write_cmd(SET_PAGE_ADDR)
-        self.write_cmd(0)
-        self.write_cmd(self.pages - 1)
-        self.write_data(self.buffer)
-
-
-class SSD1306_I2C(SSD1306):
-    def __init__(self, width, height, addr=0x3C, external_vcc=False):
-        machine = os.uname().machine
-        if ("KidBright32" in machine) or ("KidMotor V4" in machine):
-            self.i2c = I2C(1, scl=Pin(5), sda=Pin(4), freq=400000)
-        elif ("Mbits" in machine) or ("OpenBIT" in machine):
-            self.i2c = I2C(0, scl=Pin(21), sda=Pin(22), freq=400000)
-        else:
-            self.i2c = I2C(0, scl=Pin(22), sda=Pin(21), freq=400000)
-        self.addr = addr
-        self.temp = bytearray(2)
-        self.write_list = [b"\x40", None]  # Co=0, D/C#=1
-        super().__init__(width, height, external_vcc)
-
-    def write_cmd(self, cmd):
-        self.temp[0] = 0x80  # Co=1, D/C#=0
-        self.temp[1] = cmd
-        self.i2c.writeto(self.addr, self.temp)
-
-    def write_data(self, buf):
-        self.write_list[1] = buf
-        self.i2c.writevto(self.addr, self.write_list)
-        
 #END STATE OLED
 
