93ddc0b1a2d512adedfe19b1e2173767447366fd
Thanaphon06 <68720136+Thanaphon06@users.noreply.github.com>
2023-09-08 17:20:37 +0700

    update new servo

--- blocks/generators.js
+++ blocks/generators.js
@@ -87,13 +87,32 @@
         //Motor End ///////////////////////////////////
         
         // servo Start ////////////////////////////////
-        Blockly.Python['servo_new'] = function(block) { 
-          Blockly.Python.definitions_['import_beetlecar'] = 'import beetlecar';
+       Blockly.Python['servo'] = function(block) { //ชื่อที่เราตั้งไว้
           
-          var value_pin = block.getFieldValue('pin');
-          var value_angle = block.getFieldValue('angle');
-          
-          var code = `beetlecar.servo(${value_pin}, ${value_angle})\n`;
+          Blockly.Python.definitions_['from_machine_import_pin_pwm'] = 'from machine import Pin,PWM';
+          var value_pin = block.getFieldValue('pin');//เรียกค่าจาก %1
+          var value_angle = block.getFieldValue('angle');//เรียกค่า %2
+    
+          var code = ``; //ส่วนโค๊ดbeetlecar.servo(${value_pin}, ${value_angle})\n
+          if (value_pin == '16') {
+            code = `beetlecar.servo_LiftUp(value_angle)\n`
+          if(value_angle >= 90){
+            code = `beetlecar.servo_LiftUp(90)\n`
+            }
+          if(value_angle <= 0){
+            code = `beetlecar.servo_LiftUp(0)\n`
+          }
+        }
+          else if(value_pin == '17'){
+            code = `beetlecar.servo_Clamp(value_angle)\n`
+          if(value_angle >= 90){
+            code = `beetlecar.servo_Clamp(90)\n`
+          }
+          if(value_angle <= 0){
+            code = `beetlecar.servo_Clamp(0)\n`
+          }
+        }
+    
           return code;
         };
         // servo End //////////////////////////////////
