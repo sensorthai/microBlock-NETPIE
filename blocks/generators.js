bb3546edce4f5e4870b7b8a9b4ae250107ddbbd1
Thanaphon06 <68720136+Thanaphon06@users.noreply.github.com>
2023-09-08 13:55:07 +0700

    ีupdate led

--- blocks/generators.js
+++ blocks/generators.js
@@ -636,3 +636,29 @@ Blockly.Python['newstopMotor'] = function(block) {
   
   return code;
 };
+Blockly.Python['led_left'] = function(block) {
+          
+            Blockly.Python.definitions_['import_beetlecar'] = 'import beetlecar';
+          
+              var dropdown_trig = block.getFieldValue('pin_trig');
+              code = '';;
+              if(dropdown_trig == 1){
+                code = `beetlecar.led_left_on()\n`;
+              }else if(dropdown_trig == 2){
+                code = `beetlecar.led_left_off()\n`;
+              }
+              return code;
+            };
+Blockly.Python['led_right'] = function(block) {
+          
+Blockly.Python.definitions_['import_beetlecar'] = 'import beetlecar';
+            
+            var dropdown_trig = block.getFieldValue('pin_trig');
+                code = '';;
+                if(dropdown_trig == 3){
+                  code = `beetlecar.led_right_on()\n`;
+                }else if(dropdown_trig == 4){
+                  code = `beetlecar.led_right_off()\n`;
+                }
+                return code;
+              };
